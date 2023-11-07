c83776bf6ed0c316e19eef197dae15e09dc86bbf
rikyyardi <rikyardi17@gmail.com>
2023-11-07 11:11:17 +0700

    first commit

---
+++ crud_django/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for crud_django project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from notes import views
+
+urlpatterns = [
+    path('', views.note_list, name='note_list'),
+    path('note/<int:pk>/', views.note_detail, name='note_detail'),
+    path('note/new/', views.note_create, name='note_create'),
+    path('note/<int:pk>/edit/', views.note_update, name='note_update'),
+    path('note/<int:pk>/delete/', views.note_delete, name='note_delete'),
+]
+
